b60c881ce3a327fbcef2e6b15dc2a504ce88dcb0
Zeeshan <zeeshan9402529@gmail.com>
2023-12-20 22:44:46 +0500

    sign in form ui

---
+++ frontend/src/components/InputField.vue
@@ -0,0 +1,37 @@
+<template>
+  <input
+    class="outline-none border-b py-2 px-2 w-3/4"
+    :type="type"
+    :name="name"
+    :placeholder="placeholder"
+    @input="inputHandler"
+  />
+</template>
+
+<script setup lang="ts">
+import { defineProps } from "vue";
+
+const emit = defineEmits(['updateValue'])
+defineProps({
+  type: {
+    required: true,
+    type: String,
+    default: "text",
+  },
+  name: {
+    required: true,
+    type: String,
+    default: "text",
+  },
+  placeholder: {
+    required: false,
+    type: String,
+    default: "Enter Text",
+  },
+});
+
+const inputHandler = (event) => {
+        emit('updateValue', event);
+    }
+</script>
+
--- frontend/src/views/LoginView.vue
+++ frontend/src/views/LoginView.vue
@@ -1,15 +1,55 @@
 <template>
-    <div>
-       <h1>
-        Login view
-       </h1>
-    </div>
+  <div class="flex flex-col  items-center justify-center h-screen">
+      <form
+      @submit.prevent="login"
+      action="login"
+      class="flex flex-col gap-2 w-1/2 border p-4 border-red-200 items-center justify-center"
+      >
+      <h2 class="text-xl font-semibold">
+          Login Form
+      </h2>
+      <template v-for="{ name, type, placeholder } in loginForm" :key="name">
+        <input-field
+          :type="type"
+          :placeholder="placeholder"
+          :name="name"
+          @update-value="changeHandler"
+        />
+      </template>
+      <button class="bg-weather-primary w-1/2 rounded-md py-2 text-center text-white outline-none focus:outline-none">Login</button>
+    </form>
+  </div>
 </template>
 
 <script setup lang="ts">
+import { reactive, ref } from "vue";
+import { InputField } from "../components";
+function login() {
+  console.log("submit login");
+  console.log(formValues, "formValues");
+}
+const loginForm = [
+  {
+    type: "text",
+    name: "email",
+    placeholder: "Email:",
+  },
+  {
+    type: "password",
+    name: "password",
+    placeholder: "Password",
+  },
+];
 
+const formValues = reactive({
+  email: "",
+  password: "",
+});
+
+const changeHandler = (e: any) => {
+  const { name, value } = e.target;
+  formValues[name] = value;
+};
 </script>
 
-<style lang="scss" scoped>
-
-</style>
+<style lang="scss" scoped></style>
