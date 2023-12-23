5ae13f08bcd62f6f185e1a88667a0724a066274f
Zeeshan <zeeshan9402529@gmail.com>
2023-12-23 18:51:35 +0500

    admin login and login form validation

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -2,17 +2,19 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { CorsOptions } from '@nestjs/common/interfaces/external/cors-options.interface';
 import { ValidationPipe } from '@nestjs/common';
+import * as cors from 'cors';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
   
-  const corsOptions: CorsOptions = {
-    origin: 'http://localhost:3000',
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-    credentials: true,
-    optionsSuccessStatus: 204,
-  };
-  app.enableCors(corsOptions);
+  // const corsOptions: CorsOptions = {
+  //   origin: 'http://localhost:3000',
+  //   methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+  //   credentials: true,
+  //   optionsSuccessStatus: 204,
+  // };
+  // app.enableCors(corsOptions);
+  app.enableCors();
   await app.listen(3000);
 }
 bootstrap();
--- frontend/src/views/LoginView.vue
+++ frontend/src/views/LoginView.vue
@@ -1,22 +1,36 @@
 <template>
-  <div class="flex flex-col  items-center justify-center h-screen">
-      <form
+  <div class="flex flex-col items-center justify-center h-screen">
+    <form
       @submit.prevent="login"
       action="login"
-      class="flex flex-col gap-2 w-1/2 border p-4 border-red-200 items-center justify-center"
-      >
-      <h2 class="text-xl font-semibold">
-          Login Form
-      </h2>
+      class="flex flex-col gap-2 w-1/2 border p-2 border-red-200 items-center justify-center"
+    >
+      <h2 class="text-xl font-semibold">Login Form</h2>
       <template v-for="{ name, type, placeholder } in loginForm" :key="name">
         <input-field
           :type="type"
           :placeholder="placeholder"
           :name="name"
-          @update-value="changeHandler"
+          @update-value="handleChange"
         />
+        <p
+          v-if="validationError[name].length > 0"
+          class="text-red-500 !text-left"
+        >
+          {{ validationError[name] }}
+        </p>
       </template>
-      <button class="bg-weather-primary w-1/2 rounded-md py-2 text-center text-white outline-none focus:outline-none">Login</button>
+      <button
+        :disabled="
+          validationError.email.length > 0 ||
+          validationError.password.length > 0 ||
+          formValues.email.length === 0 ||
+          formValues.password.length === 0
+        "
+        class="bg-weather-primary w-1/2 rounded-md py-2 text-center text-white outline-none focus:outline-none"
+      >
+        Login
+      </button>
     </form>
   </div>
 </template>
@@ -24,10 +38,41 @@
 <script setup lang="ts">
 import { reactive, ref } from "vue";
 import { InputField } from "../components";
-function login() {
+import axios from "axios";
+import { useToast } from "vue-toast-notification";
+import "vue-toast-notification/dist/theme-sugar.css";
+const $toast = useToast();
+
+const login = async () => {
   console.log("submit login");
-  console.log(formValues, "formValues");
-}
+  const requestOptions = {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify({
+      email: formValues.email,
+      password: formValues.password,
+    }),
+  };
+  const response = await fetch(
+    "http://192.168.100.16:3000/auth/admin/sign-in",
+    requestOptions
+  );
+  const data = await response.json();
+  console.log(response, "dataaaaaaaaaa")
+  if(response.ok) {
+    console.log(response, "response");
+    $toast.success("Logged in!", {
+      duration: 3000,
+      position: "top-right",
+    });
+  } else {
+    $toast.error(data.error.message, {
+      duration:3000,
+      position:"top-right"
+    })
+  }
+  console.log(login, "login");
+};
 const loginForm = [
   {
     type: "text",
@@ -46,9 +91,24 @@ const formValues = reactive({
   password: "",
 });
 
-const changeHandler = (e: any) => {
+const validationError = reactive({
+  email: "",
+  password: "",
+});
+
+const handleChange = (e) => {
   const { name, value } = e.target;
   formValues[name] = value;
+  switch (name) {
+    case "email":
+      if (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(value)) {
+        validationError.email = "";
+      } else {
+        validationError.email = "email is not valid!";
+      }
+      break;
+  }
+  console.log(validationError.email, "email error");
 };
 </script>
 
